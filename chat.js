b1182124b8fe49543003dac755e8b548602b4900
Markoronie1 <markchernolutskiy@gmail.com>
2025-05-28 22:27:18 -0400

    fixed avatar/message logic and reordered chat.js

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
 const messages = [];
+let currentUser = 'user1';
 
 // emoji button to switch users (change to login system later or sum, add acc workign emojis)
-let currentUser = 'user1';
 document.getElementById('emojiBtn').addEventListener('click', () => {
   if (currentUser === 'user1') {
   currentUser = 'user2';
@@ -17,8 +17,9 @@ const messageInput = document.getElementById('messageInput');
 const sendBtn = document.getElementById('sendBtn');
 
 function renderMessages() {
-  chatBox.innerHTML = ''; // removes existing messages (ignoire since message are local for now, REMOVE when perm storage)
-  messages.forEach(msg => {
+  chatBox.innerHTML = ''; // clears all messages
+
+  messages.forEach((msg, index) => { //ts then repopulates
     const messageEl = document.createElement('div');
     if (msg.user === 'user1') {
         messageEl.className = 'message right';
@@ -26,16 +27,17 @@ function renderMessages() {
         messageEl.className = 'message left';
     }
 
-
     const bubble = document.createElement('div');
     bubble.className = 'bubble';
     bubble.innerText = msg.text;
-
-    const avatar = document.createElement('div');
-    avatar.className = 'avatar';
-
     messageEl.appendChild(bubble);
-    messageEl.appendChild(avatar);
+
+    const isLastInGroup = index === messages.length - 1 || messages[index + 1].user !== msg.user; //if last or diff user
+    if (isLastInGroup) {
+      const avatar = document.createElement('div');
+      avatar.className = 'avatar';
+      messageEl.appendChild(avatar);
+    }
 
     chatBox.appendChild(messageEl);
   });
